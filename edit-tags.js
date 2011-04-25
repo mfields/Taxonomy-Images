033a82c05f3ffef72bc9d33814dc873bf0c31154
Michael Fields <michael@mfields.org>
2011-04-25 07:07:48 -0700

    Move hovers to stylesheet. Kill evil carriage returns.

--- edit-tags.js
+++ edit-tags.js
@@ -1,40 +1,27 @@
 jQuery( document ).ready( function( $ ) {
-	$( '.taxonomy-image-control .control' ).hover(
-		function() { $( this ).css( 'cursor', 'pointer' ); },
-		function() { $( this ).css( 'cursor', 'default' ); }
-		);
-	$( '.taxonomy-image-control .upload' ).hover(
-		function() { $( this ).css( 'background-position', '-15px 0' ); },
-		function() { $( this ).css( 'background-position', '0 0' ); }
-		);
-	$( '.taxonomy-image-control .remove' )
-		.hover(
-			function() { $( this ).css( 'background-position', '-45px 0' ); },
-			function() { $( this ).css( 'background-position', '-30px 0' ); }
-			)
-		.click( function () {
-			var term_taxonomy_id = parseInt( $( this ).attr( 'rel' ) );
-			$.ajax({
-				url: ajaxurl,
-				type: "POST",
-				dataType: 'json',
-				data: {
-					'action' : 'taxonomy_image_plugin_remove_association',
-					'wp_nonce' : taxonomyImagesPluginEditTags.nonce,
-					'term_taxonomy_id' : term_taxonomy_id
-					},
-				cache: false,
-				success: function ( data ) {
-					data = eval( data );
-					if ( 'good' === data.status ) {
-						$( '#remove-' + term_taxonomy_id ).addClass( 'hide' );
-						$( '#taxonomy_image_plugin_' + term_taxonomy_id ).attr( 'src', taxonomyImagesPluginEditTags.img_src );
-					}
-					else if ( 'bad' === data.status ) {
-						alert( data.why );
-					}
+	$( '.taxonomy-image-control .remove' ).click( function () {
+		var term_taxonomy_id = parseInt( $( this ).attr( 'rel' ) );
+		$.ajax({
+			url: ajaxurl,
+			type: "POST",
+			dataType: 'json',
+			data: {
+				'action' : 'taxonomy_image_plugin_remove_association',
+				'wp_nonce' : taxonomyImagesPluginEditTags.nonce,
+				'term_taxonomy_id' : term_taxonomy_id
+				},
+			cache: false,
+			success: function ( data ) {
+				data = eval( data );
+				if ( 'good' === data.status ) {
+					$( '#remove-' + term_taxonomy_id ).addClass( 'hide' );
+					$( '#taxonomy_image_plugin_' + term_taxonomy_id ).attr( 'src', taxonomyImagesPluginEditTags.img_src );
 				}
-			});
-			return false;
-		} );
+				else if ( 'bad' === data.status ) {
+					alert( data.why );
+				}
+			}
+		});
+		return false;
+	} );
 } );
