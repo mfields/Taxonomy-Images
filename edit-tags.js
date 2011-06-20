c61daa8508423b31ac7b533aab18c473000d4da3
Michael Fields <michael@mfields.org>
2011-06-20 08:39:21 -0700

    Enable delete button from edit-terms.php. Reported by xSEOn on wordpress.mfields.org

--- edit-tags.js
+++ edit-tags.js
@@ -6,8 +6,7 @@ jQuery( document ).ready( function( $ ) {
 		taxonomyImagesPlugin.image_id = parseInt( $( this ).parent().find( 'input.image_id' ).val() );
 	} );
 
-	$( '.taxonomy-images-modal .remove' ).live( 'click', function () {
-		var tt_id = parseInt( $( this ).attr( 'rel' ) );
+	$( '.taxonomy-image-control .remove' ).live( 'click', function () {
 		$.ajax( {
 			url: ajaxurl,
 			type: "POST",
@@ -15,13 +14,13 @@ jQuery( document ).ready( function( $ ) {
 			data: {
 				'action'   : 'taxonomy_image_plugin_remove_association',
 				'wp_nonce' : taxonomyImagesPlugin.nonce,
-				'tt_id'    : tt_id
+				'tt_id'    : taxonomyImagesPlugin.tt_id
 				},
 			cache: false,
 			success: function ( response ) {
 				if ( 'good' === response.status ) {
-					$( '#remove-' + tt_id ).addClass( 'hide' );
-					$( '#taxonomy_image_plugin_' + tt_id ).attr( 'src', taxonomyImagesPlugin.img_src );
+					$( '#remove-' + taxonomyImagesPlugin.tt_id ).addClass( 'hide' );
+					$( '#taxonomy_image_plugin_' + taxonomyImagesPlugin.tt_id ).attr( 'src', taxonomyImagesPlugin.img_src );
 				}
 				else if ( 'bad' === response.status ) {
 					alert( response.why );
